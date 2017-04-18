73a789fb8018fa35e53d21ccaf12b0772abfa3fb
Matteo Corradini <corra.matteo@gmail.com>
2017-04-18 13:37:01 +0200

    Add resize_partition.py support to old script

---
+++ clean_write_image.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo umount "$1"1
+sudo umount "$1"2
+
+sudo parted -s "$1" rm 1
+sudo parted -s "$1" rm 2
+
+sudo dd if=output/images/sdcard.img of="$1" bs=1M
+
+./resize_partition "$1"
