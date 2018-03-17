1f995ebd7f4e6f433f0890815b020a3830dbc7f3
CorraMatte <corra.matteo@gmail.com>
2018-03-17 11:11:47 +0100

    Update absolute path

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -9,7 +9,7 @@ PCAN_DIR="${TARGET_DIR}"/lib/modules/"${KERNEL_VERSION}"/kernel/drivers/peak/
 
 cd "${BUILD_DIR}"/peak-driver-"${DRIVER_VERSION}"/
 make clean
-make -C driver PCIEC=NO PCI=NO ISA=NO PCC=NO DNG=NO KERNEL_LOCATION=/home/corra/buildroot-2017.02/output/build/linux-84a1781639429747b894f5121476b514a6a20651/ ARCH=arm CROSS_COMPILE=/home/corra/buildroot-2017.02/output/host/usr/bin/arm-buildroot-linux-uclibcgnueabihf-
+make -C driver PCIEC=NO PCI=NO ISA=NO PCC=NO DNG=NO KERNEL_LOCATION="${BUILD_DIR}"/linux-84a1781639429747b894f5121476b514a6a20651/ ARCH=arm CROSS_COMPILE="${HOST_DIR}"/usr/bin/arm-buildroot-linux-uclibcgnueabihf-
 
 if [ ! -d "$PCAN_DIR" ]; then
 	echo "here" 
